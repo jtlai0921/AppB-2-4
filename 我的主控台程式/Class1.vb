218cc269847e91dded095d89be557e7805db2359
賴紀廷 <49492695+jtlai0921@users.noreply.github.com>
2020-01-17 16:50:45 +0800

    Initial commit

---
+++ 我的主控台程式/Class1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Student
+    Private Name As String
+    Private Shared Count As Integer
+    ' 建構子
+    Public Sub New(ByVal Name As String)
+        Me.Name = Name
+        Count += 1
+    End Sub
+    ' 成員方法
+    Public Function GetStudentName() As String
+        Return Name
+    End Function
+    ' 靜態成員方法，也稱為類別方法
+    Public Shared Function NumOfStudents() As Integer
+        Return Count
+    End Function
+End Class
+
+
---
+++ 我的主控台程式/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        ' 宣告Student類別的物件變數
+        Dim joe, jane, jason As Student
+        ' 建立物件實例
+        joe = New Student("陳會安")
+        jane = New Student("江小魚")
+        jason = New Student("陳允傑")
+        ' 呼叫物件方法
+        txtOutput.Text = "姓名: " & joe.GetStudentName() & vbNewLine
+        txtOutput.Text &= "姓名: " & jane.GetStudentName() & vbNewLine
+        txtOutput.Text &= "姓名: " & jason.GetStudentName() & vbNewLine
+        txtOutput.Text &= "學生數: " & Student.NumOfStudents()
+        txtOutput.SelectionStart = 0
+    End Sub
+End Class
